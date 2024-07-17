99cc75c8f1dad9dd066df1d3fbc09e001431cb51
Hannah Kania <hpk4@dcc-core-35.rc.duke.edu>
2024-07-17 16:12:11 -0400

    editing files on cluster

--- simulations/scripts/artificial_seq/hg38.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem=10G  # adjust as needed
-#SBATCH --partition=scavenger
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=kania.hannah@duke.edu
-
-perl bin/createModel.pl -v -m hum38 -f /cwork/hpk4/genome_UCSC_test/hg38/hg38.fa -r /cwork/hpk4/genome_UCSC_test/hg38/hg38.fa.align -t /cwork/hpk4/genome_UCSC_test/hg38/hg38.trf.bed -g /cwork/hpk4/genome_UCSC_test/hg38/hg38.ensGene.gtf
